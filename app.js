84d816b4140929dd858309162547859ecb8200be
GaryHuu <nguyenhuu220800@gmail.com>
2021-05-11 16:29:27 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var slider = document.getElementById('slider');
+var selector = document.getElementById('selector');
+var SelectValue = document.getElementById('SelectValue');
+var ProgressBar = document.getElementById('ProgressBar');
+SelectValue.innerHTML = slider.value;
+slider.oninput = function (e) {
+  console.log(e.target);
+  SelectValue.innerHTML = this.value;
+  selector.style.left = this.value + '%';
+  ProgressBar.style.width = this.value + '%';
+};
